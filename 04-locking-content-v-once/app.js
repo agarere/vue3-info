d49bfe9890e54032618521ae41a35f6b90455f9f
agarere <edizagarer@gmail.com>
2024-05-21 09:55:10 +0300

    Added v-once example

---
+++ 04-locking-content-v-once/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add (num = 1) {
+      this.counter = this.counter + num
+    },
+
+    reduce (num = 1) {
+      this.counter = this.counter - num
+    }
+  }
+});
+
+app.mount('#events')
